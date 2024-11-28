9fd62700ba4d251db9664f7ad1c72cb6f416a2d1
nicosolano <nicoesolano@gmail.com>
2024-11-28 17:19:44 -0300

    admin canciones

--- admin/views/admin_canciones.php
+++ admin/views/admin_canciones.php
@@ -1,6 +1,10 @@
 <?php
 
 $canciones = (new Cancion())->todas_las_canciones();
+$pag = $_GET["pag"] ?? 1;
+$paginas = intdiv(count($canciones), 25);
+$inicio = ($pag - 1) * 25;
+$fin = $inicio + 25;
 
 ?>
 
@@ -8,7 +12,7 @@ $canciones = (new Cancion())->todas_las_canciones();
     <div class="row my-5">
         <div class="col">
             <h1 class="text-center mb-5" style="color: #543310;">Administración de Canciones</h1>
-            <div class="row mb-5 d-flex align-items-center">
+            <div class="row mb-5 d-flex justify-content-center align-items-center">
                 <table class="table">
                     <thead>
                         <tr>
@@ -19,19 +23,26 @@ $canciones = (new Cancion())->todas_las_canciones();
                         </tr>
                     </thead>
                     <tbody>
-                        <?php foreach($canciones as $c){ ?>
-                        <tr>
-                            <th scope="row"><?= $c->getId() ?></th>
-                            <td><?= $c->getTitulo() ?></td>
-                            <td><?= $c->getAlbum() ?></td>
-                            <td>
-                                <a class="btn btn-warning" href="index.php?sec=edit_cancion&id=<?= $c->getId() ?>">Editar</a>
-                                <a class="btn btn-danger" href="index.php?sec=delete_cancion&id=<?= $c->getId() ?>">Eliminar</a>
-                            </td>
-                        </tr>
+                        <?php for ($inicio; $inicio < $fin; $inicio++) { ?>
+                            <tr>
+                                <th scope="row"><?= $canciones[$inicio]->getId() ?></th>
+                                <td><?= $canciones[$inicio]->getTitulo() ?></td>
+                                <td><?= $canciones[$inicio]->getAlbum() ?></td>
+                                <td>
+                                    <a class="btn btn-warning" href="index.php?sec=edit_cancion&id=<?= $canciones[$inicio]->getId() ?>">Editar</a>
+                                    <a class="btn btn-danger" href="index.php?sec=delete_cancion&id=<?= $canciones[$inicio]->getId() ?>">Eliminar</a>
+                                </td>
+                            </tr>
                         <?php } ?>
                     </tbody>
                 </table>
+                <ul class="list-group list-group-horizontal d-flex justify-content-center">
+                    <?php for($pag; $pag <= $paginas; $pag++){ ?>
+                        <a href="index.php?sec=admin_canciones&pag=<?= $pag ?>">
+                            <li class="list-group-item"><?= $pag ?></li>
+                        </a>
+                    <?php } ?>
+                </ul>
                 <a class="btn btn-primary mt-5" href="index.php?sec=add_cancion">Cargar Nueva Canción</a>
             </div>
         </div>
