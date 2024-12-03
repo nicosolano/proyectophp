2e824bcdb0e786a0855dfc001907bb3fe0bbd7a8
nicosolano <nicoesolano@gmail.com>
2024-12-03 17:14:52 -0300

    filtrar por álbum

--- admin/views/admin_canciones.php
+++ admin/views/admin_canciones.php
@@ -1,6 +1,13 @@
 <?php
 
-$canciones = (new Cancion())->todas_las_canciones();
+$albumes = (new Album())->todos_los_albumes();
+$id_album = $_POST["id_album"] ?? NULL;
+if(isset($id_album) && $id_album !== "0"){
+    $canciones = (new Cancion())->canciones_x_album($id_album);
+}else{
+    $canciones = (new Cancion())->todas_las_canciones();
+}
+
 $pag = $_GET["pag"] ?? 1;
 $limite = 25;
 $paginas = intdiv(count($canciones), $limite) + 1;
@@ -16,6 +23,18 @@ $fin = $inicio + $limite;
             <div>
                 <?= (new Alerta())->get_alertas() ?>
             </div>
+            <div class="row mb-5 d-flex align-items-center">
+                <form class="row g-3" action="index.php?sec=admin_canciones" method="POST">
+                    <label class="form-label" for="id_album">Filtrar por álbum:</label>
+                    <select class="form-select" name="id_album" id="id_album" required>
+                        <option value="0" selected>Todos los álbumes</option>
+                        <?php foreach ($albumes as $a) { ?>
+                            <option value="<?= $a->getId() ?>"><?= $a->getTitulo() ?></option>
+                        <?php } ?>
+                    </select>
+                    <button class="btn btn-primary" type="submit">Buscar</button>
+                </form>
+            </div>
             <div class="row mb-5 d-flex justify-content-center align-items-center">
                 <table class="table">
                     <thead>
@@ -28,7 +47,7 @@ $fin = $inicio + $limite;
                     </thead>
                     <tbody>
                         <?php for ($inicio; $inicio < $fin; $inicio++) { ?>
-                            <?php if(isset($canciones[$inicio])){ ?>
+                            <?php if (isset($canciones[$inicio])) { ?>
                                 <tr>
                                     <th scope="row" id="<?= $canciones[$inicio]->getId() ?>"><?= $canciones[$inicio]->getId() ?></th>
                                     <td><?= $canciones[$inicio]->getTitulo() ?></td>
@@ -43,7 +62,7 @@ $fin = $inicio + $limite;
                     </tbody>
                 </table>
                 <ul class="list-group list-group-horizontal d-flex justify-content-center">
-                    <?php for($pag=1; $pag <= $paginas; $pag++){ ?>
+                    <?php for ($pag = 1; $pag <= $paginas; $pag++) { ?>
                         <a href="index.php?sec=admin_canciones&pag=<?= $pag ?>">
                             <li class="list-group-item"><?= $pag ?></li>
                         </a>
